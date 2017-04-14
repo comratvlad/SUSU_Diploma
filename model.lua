dbe187b6958cc565d8a074f87041cd7f9ad527f3
Vladislav <comratvlad@yandex.ru>
2017-04-14 21:25:27 +0500

    Update model.lua

--- model.lua
+++ model.lua
@@ -1,9 +1,9 @@
 -- =======================================================
 -- model.lua
 --
--- Модуль конструирования нейронных сетей, для 
+-- Модуль конструирования нейронных сетей, для
 -- распознания антропометрических точек лица
--- Facial Keypoints Detection 
+-- Facial Keypoints Detection и 300wlp
 --
 -- =======================================================
 
@@ -17,13 +17,10 @@ function model.createSimpleCNN()
   
   net = nn.Sequential()
 
-  
   net:add(nn.SpatialConvolutionMM(1, 16, 3, 3))
 
   net:add(nn.ReLU(false))
   net:add(nn.SpatialMaxPooling(2, 2, 2, 2))
-
-
 
   net:add(nn.SpatialConvolutionMM(16, 32, 3, 3))
   net:add(nn.ReLU(false))
@@ -46,7 +43,7 @@ function model.makeDeepCNN(mm)
 	mm = mm or 1
 
 	local result = {}
-  	local iv = inception_variants(mm);
+    local iv = inception_variants(mm);
   
 	result.network = construct_structure({'seq',
       {'conv', 1, 4, 3, 3, 1, 1, 0, 0}, {'activation', 4},
@@ -70,27 +67,109 @@ function model.makeDeepCNN(mm)
         -- {'wv'}, -- 128x1x1
     })
 
-    	result.network:add(nn.View(512))
-    	result.network:add(nn.Dropout(0.4))
+    result.network:add(nn.View(512))
+    result.network:add(nn.Dropout(0.4))
 
-    	result.network:add(nn.Linear(512, 256));
-   	result.network:add(nn.View(16));
-   	result.network:add(nn.Normalize(2));
-   	result.network:add(nn.View(256));
+    result.network:add(nn.Linear(512, 256));
+    result.network:add(nn.View(16));
+    result.network:add(nn.Normalize(2));
+    result.network:add(nn.View(256));
 
-	result.network:add(nn.Linear(256, 128));
-   	result.network:add(nn.BatchNormalization(128));
-   	result.network:add(nn.ReLU(false))
+    result.network:add(nn.Linear(256, 128));
+    result.network:add(nn.BatchNormalization(128));
+    result.network:add(nn.ReLU(false))
 
-   	result.network:add(nn.Linear(128, 30));
-   	result.network:add(nn.View(2, 15));
+    result.network:add(nn.Linear(128, 30));
+    result.network:add(nn.View(2, 15));
   
-  	local x = torch.Tensor():randn(8, (SOURCE_CHANNELS_COUNT or 1), 96, 96)
+    local x = torch.Tensor():randn(8, (SOURCE_CHANNELS_COUNT or 1), 96, 96)
 
 	result.input_size = drop_minibatch_size( x:size() )
 	result.output_size = drop_minibatch_size( result.network:forward(x):size() )
 
 	return result;
+end
+---------------------------------------------------------
+
+-- Первый вариант сети для выборки 300wlp
+function model.createSimple300wlpCNN()
+
+    net = nn.Sequential()
+
+    net:add(nn.SpatialConvolutionMM(1, 16, 3, 3))
+
+    net:add(nn.ReLU(false))
+    net:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+
+    net:add(nn.SpatialConvolutionMM(16, 32, 3, 3))
+    net:add(nn.ReLU(false))
+    net:add(nn.SpatialMaxPooling(2, 2, 2, 2))
+
+    net:add(nn.View(32*51*51))
+    net:add(nn.Dropout(0.4))
+
+    net:add(nn.Linear(32*51*51, 200))
+    net:add(nn.BatchNormalization(200));
+    net:add(nn.ReLU(false))
+    net:add(nn.Linear(200, 2*68))
+    net:add(nn.View(2,68))
+
+    return net
+
+end
+---------------------------------------------------------
+
+-- Второй вариант сети для выборки 300wlp
+function model.makeDeep300wlpCNN(mm)
+
+    mm = mm or 1
+
+    local result = {}
+    local iv = inception_variants(mm);
+
+    result.network = construct_structure({'seq',
+        {'conv', 1, 4, 3, 3, 1, 1, 0, 0}, {'activation', 4},
+        {'conv', 4, 8, 3, 3, 1, 1, 0, 0}, {'activation', 8},
+        --{'maxpool', 2, 2,  2, 2,  1, 1},
+
+        {'conv', 8, 16, 3, 3, 1, 1, 0, 0}, {'activation', 16},
+        {'maxpool', 2, 2,  2, 2,  1, 1},
+
+        {'conv', 16, 32, 3, 3, 1, 1, 0, 0}, {'activation', 32},
+        {'maxpool', 2, 2,  2, 2,  1, 1},
+        --32x24x24
+
+        iv.inception1ab,  -- {'wv'},  -- 32x24x24
+        iv.inception2,    -- {'wv'},  -- 64x12x12
+        iv.inception3ab,  -- {'wv'},  -- 64x12x12
+        iv.inception4,    -- {'wv'},  -- 128x6x6
+        iv.inception5abc, -- {'wv'},  -- 128x6x6
+
+        {'conv', 128*13*13*mm, 512, 6, 6, 1, 1, 0, 0}, {'activation', 512},
+        -- {'wv'}, -- 128x1x1
+    })
+
+    result.network:add(nn.View(512))
+    result.network:add(nn.Dropout(0.4))
+
+    result.network:add(nn.Linear(512, 256));
+    result.network:add(nn.View(16));
+    result.network:add(nn.Normalize(2));
+    result.network:add(nn.View(256));
+
+    result.network:add(nn.Linear(256, 128));
+    result.network:add(nn.BatchNormalization(128));
+    result.network:add(nn.ReLU(false))
+
+    result.network:add(nn.Linear(128, 2*68));
+    result.network:add(nn.View(2, 68));
+
+    local x = torch.Tensor():randn(8, (SOURCE_CHANNELS_COUNT or 1), 96, 96)
+
+    result.input_size = drop_minibatch_size( x:size() )
+    result.output_size = drop_minibatch_size( result.network:forward(x):size() )
+
+    return result;
 end
 ---------------------------------------------------------
 
