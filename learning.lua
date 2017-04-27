d4fd3a15cb28bdff48ee1cd11985264467580950
Vladislav <comratvlad@yandex.ru>
2017-04-27 17:07:10 +0500

    Update learning.lua

--- learning.lua
+++ learning.lua
@@ -15,12 +15,18 @@ require 'torch'
 require 'optim'
 require 'nn'
 
-require 'cunn'
-require 'cutorch
 require 'cudnn'
+require 'cunn'
+require 'cutorch'
+require 'xlua'
 
+torch.setdefaulttensortype('torch.FloatTensor');
+
+-- Инициализация выборки
 local count, test_count = c_init()
 print('Данные выборки загружены...')
+
+-- Параметры обучения
 local batchsize = 16
 local rows = 210
 local cols = 210
@@ -32,8 +38,19 @@ local optimState = {
     weightDecay = 1e-6,
     momentum = 0.9
 }
-local parameters,gradParameters = net:getParameters()
 
+-- Переходим под cuda
+net:cuda()
+criterion:cuda()
+
+local parameters, gradParameters = net:getParameters()
+
+-- -- Выделение памяти
+local input = torch.FloatTensor(batchsize, 1, rows, cols)
+local target = torch.FloatTensor(batchsize, 2, 68)
+local input_c = torch.CudaTensor(batchsize, 1, rows, cols)
+local target_c = torch.CudaTensor(batchsize, 2, 68)
+local result = torch.FloatTensor(batchsize, 2, 68)
 
 print('Начинаем обучение...')
 
@@ -45,25 +62,23 @@ for it=1,epoch do
     -- Подготовление данных выборки (в т.ч. аугментация)
     c_prepare_iteration()
 
+    xlua.progress(0, count)
+
     -- Идем батчами по выборке
     for i=1,count,batchsize do
-
-        -- Выделение памяти
-        local input = torch.FloatTensor(batchsize, 1, rows, cols)
-        local target = torch.FloatTensor(batchsize, 2, 68)
 
         -- Получение подготовленных данных выборки
         for j=1,batchsize do
             c_get_data(i, j, input[j], target[j])
         end
 
-        input =input:double()/255.0
-        target = target:double()/210.0
+        -- Нормализация обучающих данных
+        input:div(255.0)
+        target:div(210.0)
 
-        --for j=1,batchsize do
-        --   dfun.showPointedPic(input[j], target[j]*210.0)
-        --   io.read()
-        --end
+        -- Копирование в cuda-буфер
+        input_c:copy(input)
+        target_c:copy(target)
 
         -- Непосредственно обучение
         local feval = function(x)
@@ -74,45 +89,62 @@ for it=1,epoch do
 
             gradParameters:zero()
 
-            local outputs = net:forward(input)
+            local outputs = net:forward(input_c)
 
-            local loss = criterion:forward(outputs, target)
-            local dloss_doutputs = criterion:backward(outputs, target)
-            net:backward(input, dloss_doutputs)
+            local loss = criterion:forward(outputs, target_c)
+            local dloss_doutputs = criterion:backward(outputs, target_c)
+            net:backward(input_c, dloss_doutputs)
 
             return loss, gradParameters
         end
 
         optim.sgd(feval, parameters, optimState)
 
+        xlua.progress(i + batchsize - 1, count)
+
     end
 
-    -- Тест
-    local test_result = 0.0
-    for i=1,test_count,batchsize do
+    xlua.progress(count, count)
 
-        -- Выделение памяти
-        local input = torch.FloatTensor(batchsize, 1, rows, cols)
-        local target = torch.FloatTensor(batchsize, 2, 68)
+    -- Тест
+    xlua.progress(0, test_count)
+
+    local test_result = 0.0
+
+    for i=1,test_count,batchsize do
 
         -- Получение подготовленных данных выборки
         for j=1,batchsize do
             c_get_test_data(i, j, input[j], target[j])
         end
 
-        local result = net:forward(input:double()/255.0)*210.0
+        input:div(255.0)
+        input_c:copy(input)
+
+        local result_c = net:forward(input_c)
+        result_c:mul(210.0)
+        result:copy(result_c)
 
         for j=1,batchsize do
-            test_result = test_result + dfun.e_norm(result[j], target:double()[j])
+            test_result = test_result + dfun.e_norm(result[j], target[j])
         end
+
+        xlua.progress(i + batchsize - 1, test_count);
+
     end
+
+    xlua.progress(test_count, test_count)
+
     print('Результат тестирования = ', test_result)
+
+    collectgarbage();
 
     -- Основываясь на результатах теста - меняем шаг
     print('Новый шаг:')
     new_rate = io.read()
     if (new_rate == 'end') then break end
-    optimState = {learningRate = tonumber(new_rate),
+    optimState = {
+        learningRate = tonumber(new_rate),
         weightDecay = 1e-6,
         momentum = 0.9
     }
@@ -122,5 +154,7 @@ end
 -- Сохранение результатов
 print('Обучение завершено, сохранять результат?')
 local answer = io.read()
-if answer == 'yes' then torch.save('net/simple300wlpVer22CNN.t7', net) end
+if (answer == 'yes') then
+    torch.save('net/simple300wlpVer22CNN.t7', net)
+end
 print('Готово')
